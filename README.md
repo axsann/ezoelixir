84f84e18129795c2f61778acb7f4d086d9f65ab9
axsann <axsann@users.noreply.github.com>
2015-06-20 23:38:50 +0900

    Support showing questions and answers.

---
+++ README.md
@@ -0,0 +1,22 @@
+質問ではない。
+=========
+`ezoe` command implemented with Elixir.
+
+
+## Build
+
+```
+$ mix escript.build
+```
+## Usage
+- Show questions and answers
+
+```
+$ ./ezoelixir
+```
+
+## Other `ezoe` implementations
+- [awesome-ezoe](https://github.com/mattn/awesome-ezoe)
+
+## License
+Distributed under [the MIT License](http://opensource.org/licenses/MIT)
---
+++ lib/ezoelixir.ex
@@ -0,0 +1,21 @@
+defmodule Ezoelixir do
+  import FeederEx
+  def main(args) do
+    show()
+  end
+  
+  def show() do
+    HTTPoison.start
+    {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get("http://ask.fm/feed/profile/EzoeRyou.rss")
+    {:ok, feed, _} = FeederEx.parse(body)
+    Enum.each(feed.entries,
+              fn(e) -> 
+                IO.puts("#{e.id}")
+                "  "<>String.replace(e.title, e.title, IO.ANSI.bright<>e.title<>IO.ANSI.reset)
+                |> IO.puts
+                "  "<>String.replace(e.summary, ~r/(質問ではない)(。)?|(不)?(自由)/, IO.ANSI.green<>"\\1\\2\\3\\4"<>IO.ANSI.reset)<>"\n"
+                |> IO.puts
+              end
+    )
+  end
+end
